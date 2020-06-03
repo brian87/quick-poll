1e8dfc9144aebf7b793715a43b708d0c5678624c
Braiyan <brian.aguilar.b@gmail.com>
2020-06-03 07:36:23 -0500

    add requestMapping/Polls

--- src/main/java/com/apress/controller/PollController.java
+++ src/main/java/com/apress/controller/PollController.java
@@ -1,12 +1,25 @@
 package com.apress.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.apress.domain.Poll;
 import com.apress.repository.PollRepository;
 
 @RestController
 public class PollController {
         @Autowired
         private PollRepository pollRepository;
+
+        @RequestMapping(value="/polls", method=RequestMethod.GET)
+        public ResponseEntity<Iterable<Poll>> getAllPolls() {
+                Iterable<Poll> allPolls = pollRepository.findAll();
+                return new ResponseEntity<>(allPolls, HttpStatus.OK);
+        }
 }
+
 
